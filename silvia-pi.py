66ad184fc11f929e0a56ba8056e191c0d3640daf
Dan Calacci <dcalacci@media.mit.edu>
2020-05-12 12:47:30 -0400

    lol I have no ideaa how bottle works

--- silvia-pi.py
+++ silvia-pi.py
@@ -9,7 +9,7 @@ def scheduler(dummy,state):
   sys.stdout = open("scheduler.log", "a", buffering=0)
   sys.stderr = open("scheduler.err.log", "a", buffering=0)
 
-  print "Starting scheduler thread ..."
+  print("Starting scheduler thread ...")
 
   last_wake = 0
   last_sleep = 0
@@ -191,7 +191,7 @@ def pid_loop(dummy,state):
         state['dterm'] = round(pid.DTerm * conf.Dw,2)
       state['iscold'] = iscold
 
-      print time(), state
+      print(time(), state)
 
       sleeptime = lasttime+conf.sample_time-time()
       if sleeptime < 0 :
@@ -312,28 +312,28 @@ if __name__ == '__main__':
   pidstate['settemp'] = conf.set_temp
   pidstate['avgpid'] = 0.
 
-  print "Starting Scheduler thread..."
+  print("Starting Scheduler thread...")
   s = Process(target=scheduler,args=(1,pidstate))
   s.daemon = True
   s.start()
 
-  print "Starting PID thread..."
+  print("Starting PID thread...")
   p = Process(target=pid_loop,args=(1,pidstate))
   p.daemon = True
   p.start()
 
-  print "Starting HE Control thread..."
+  print("Starting HE Control thread...")
   h = Process(target=he_control_loop,args=(1,pidstate))
   h.daemon = True
   h.start()
 
-  print "Starting REST Server thread..."
+  print("Starting REST Server thread...")
   r = Process(target=rest_server,args=(1,pidstate))
   r.daemon = True
   r.start()
 
   #Start Watchdog loop
-  print "Starting Watchdog..."
+  print("Starting Watchdog...")
   piderr = 0
   weberr = 0
   weberrflag = 0
@@ -352,7 +352,7 @@ if __name__ == '__main__':
     lasti = curi
 
     if piderr > 9 :
-      print 'ERROR IN PID THREAD, RESTARTING'
+      print('ERROR IN PID THREAD, RESTARTING')
       p.terminate()
 
     try:
@@ -367,7 +367,7 @@ if __name__ == '__main__':
       weberr = weberr + 1
 
     if weberr > 9 :
-      print 'ERROR IN WEB SERVER THREAD, RESTARTING'
+      print('ERROR IN WEB SERVER THREAD, RESTARTING')
       r.terminate()
 
     weberrflag = 0
